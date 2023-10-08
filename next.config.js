1a2a5440e270084bffb76b9ec07c4a0a4268479b
Mario Garcia <msgimanila@yahoo.com>
2023-10-08 20:52:13 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -14,16 +14,4 @@
  * limitations under the License.
  */
 
-module.exports = {
-  images: {
-    domains: [
-      'www.datocms-assets.com',
-      'a.storyblok.com',
-      'images.ctfassets.net',
-      'images.prismic.io',
-      'cdn.aglty.io',
-      'localhost' // For Strapi
-    ],
-    imageSizes: [24, 64, 300]
-  }
-};
+
