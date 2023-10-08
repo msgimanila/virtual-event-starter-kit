35dc346113d571154f71ebf7dbad7b68c45653e0
Mario Garcia <msgimanila@yahoo.com>
2023-10-08 19:48:45 +0800

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -43,7 +43,7 @@ export default function Footer() {
         </div>
         <div className={styles['footer-copyright']}>
           Copyright © {`${new Date().getFullYear()} `} {COPYRIGHT_HOLDER || `${SITE_NAME}.`} All
-          rights reserved.
+          rights reserved. 2023
         </div>
         <div className={styles['footer-center-group']}>
           <p className={styles['footer-paragraph']}>
